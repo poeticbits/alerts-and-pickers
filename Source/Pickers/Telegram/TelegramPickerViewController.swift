3f161ff1b5a3ad3759ffc893ec384caceec63a06
Zhifu Ge <zhifu@me.com>
2020-12-05 13:13:18 -0500

    Upgrade to Swift 5

--- Source/Pickers/Telegram/TelegramPickerViewController.swift
+++ Source/Pickers/Telegram/TelegramPickerViewController.swift
@@ -102,7 +102,7 @@ final class TelegramPickerViewController: UIViewController {
         $0.allowsMultipleSelection = true
         $0.showsVerticalScrollIndicator = false
         $0.showsHorizontalScrollIndicator = false
-        $0.decelerationRate = UIScrollViewDecelerationRateFast
+        $0.decelerationRate = UIScrollView.DecelerationRate.fast
         $0.contentInsetAdjustmentBehavior = .never
         $0.contentInset = UI.insets
         $0.backgroundColor = .clear
@@ -207,12 +207,12 @@ final class TelegramPickerViewController: UIViewController {
                 self.fetchPhotos(completionHandler: completionHandler)
             }
             
-        case .denied, .restricted:
+        case .denied, .restricted, .limited:
             /// User has denied the current app to access the contacts.
             let productName = Bundle.main.infoDictionary!["CFBundleName"]!
             let alert = UIAlertController(style: .alert, title: "Permission denied", message: "\(productName) does not have access to contacts. Please, allow the application to access to your photo library.")
             alert.addAction(title: "Settings", style: .destructive) { action in
-                if let settingsURL = URL(string: UIApplicationOpenSettingsURLString) {
+                if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
                     UIApplication.shared.open(settingsURL)
                 }
             }
